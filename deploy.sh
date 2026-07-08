94e6f53c8c7e202e299f868834a3912c908290b5
robingopi <robinparur@gmail.com>
2026-07-08 12:23:11 +0000

    Add webapp and deploy script

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e # Exit on any error
+VERSION=$1
+echo "[DEPLOY] Starting deployment of version $VERSION"
+# Replace placeholders in HTML
+sed -i "s/BUILD_VERSION_PLACEHOLDER/$VERSION/g" webapp/index.html
+sed -i "s/DEPLOY_TIME_PLACEHOLDER/$(date)/g" webapp/index.html
+# Copy to web server
+sudo cp webapp/index.html /var/www/html/index.html
+sudo systemctl restart httpd || sudo systemctl restart apache2
+echo "[DEPLOY] Deployment v$VERSION complete!"
+echo "[DEPLOY] App running at http://$(curl -s http://51.20.44.150/latest/meta-data/public-ipv4)"
